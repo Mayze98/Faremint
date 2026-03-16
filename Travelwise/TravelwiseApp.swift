96d1a6e4f422e3a2e3dfd156bbeecd3042293671
John Zheng <yzafc888@gmail.com>
2026-03-16 14:25:46 -0700

    Initial Commit

---
+++ Travelwise/TravelwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelwiseApp.swift
+//  Travelwise
+//
+//  Created by John on 2026-03-16.
+//
+
+import SwiftUI
+
+@main
+struct TravelwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
